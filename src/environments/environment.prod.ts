4bec734c7ef280c4fab16683dcc0abfd3e416939
unknown <marcos@datawalt.cl>
2021-09-26 21:05:45 -0300

    listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: "https://prahtgrpnbeegqlyrrqi.supabase.co",
+  supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMjY5MDM4OSwiZXhwIjoxOTQ4MjY2Mzg5fQ.i-FkKfYLI4GfroCJab96Jfg416wfq8dTecpxNeCnlLk"
 };
